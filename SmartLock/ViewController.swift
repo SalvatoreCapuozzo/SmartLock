1fe0355e12a3dc8d658482bed5cfc6dfdab07cc5
Salvatore Capuozzo <sasy.19.05.96@gmail.com>
2019-07-01 15:21:52 +0200

    First Project Commit

---
+++ SmartLock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartLock
+//
+//  Created by Salvatore Capuozzo on 01/07/2019.
+//  Copyright © 2019 Salvatore Capuozzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
